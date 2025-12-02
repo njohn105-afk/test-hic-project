5418a0d096a217bbf9baab3211ea24948594316c
njohn105-afk <njohn105@kent.edu>
2025-12-01 19:32:27 -0500

    Maybe fix

--- HicProject/settings.py
+++ HicProject/settings.py
@@ -108,8 +108,10 @@ USE_TZ = True
 # ======================
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = []
-   # for collectstatic (deployment)
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
